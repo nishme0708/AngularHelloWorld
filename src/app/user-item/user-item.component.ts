c3b3f4015eb9a4f14b60b25915e9720a08a7df84
Nishant Mehta <nishme0708@gmail.com>
2018-10-02 18:45:31 +0530

    Add user item component

---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+  @Input()
+  userName: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
